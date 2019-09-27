a9b2bd90cfd66b80abdc4627bfaf1796d0b4c964
reholliday <55766180+reholliday@users.noreply.github.com>
2019-09-27 13:17:56 -0400

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,41 @@
+# this line prints a message
+print("I will now count my chickens:")
+
+#calculates the number of hens
+print("Hens", 25 + 30 / 6)
+#calculates the number of roosters
+print("Roosters", 100 - 25 * 3 % 4)
+
+#prints message
+print("Now I will count the eggs:")
+
+#does more calculation, printing the results
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+#prints questions
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#prints the True/False answer to the line 16 question
+print(3 + 2 < 5 - 7)
+
+#performs addition
+print("What is 3 + 2?", 3 + 2)
+#performs subtraction
+print("What is 5 - 7?", 5 - 7)
+
+#prints message regarding the T/F answer
+print("Oh, that's why it's False.")
+
+#prints message
+print("How about some more.")
+
+#calculates greater than T/F
+print("Is it greater?", 5 > -2)
+#calculates greater than or equal to, T/F
+print("Is it greater or equal?", 5 >= -2)
+#calculates less than or equal to, T/F
+print("Is it less or equal?", 5 <= -2)
+
+print("I have 3 roosters and 4 hens")
+print("I have", 3 + 4, "chickens in total")
+
